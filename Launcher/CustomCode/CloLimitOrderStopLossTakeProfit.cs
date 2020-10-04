9c4df71c4857dd9041d435a9acc1264b4ac68034
KBaker <15708443+Synchronicity89@users.noreply.github.com>
2020-10-04 07:47:26 -0700

    Supports multiple picks, but bullBear is set to 1, because only bullish direction is supported.

--- Launcher/CustomCode/CloLimitOrderStopLossTakeProfit.cs
+++ Launcher/CustomCode/CloLimitOrderStopLossTakeProfit.cs
@@ -9,6 +9,12 @@ using System.Linq;
 
 namespace QuantConnect.Lean.Launcher.CustomCode
 {
+    public static class C
+    {
+        public static readonly CultureInfo en_us = new CultureInfo("en-us");
+        public const bool useTPLimit = true;
+    }
+
     public class OrderStateBase
     {
         public string ticker;
@@ -16,12 +22,15 @@ namespace QuantConnect.Lean.Launcher.CustomCode
         // used for SPY
         // public decimal limitOffset = 0.0012M;
         // public decimal stopOffset = 0.005M;
-        public const decimal ratio = 3.0m / 2.0m;
-        public decimal limitOffset = 0.018M * ratio;
-        public decimal stopOffset = 0.075M * ratio;
+        // public const decimal ratio = 1.0m/1.0m;
+        // public decimal limitOffset = 0.018M * ratio * 1.0m;
+        // public decimal stopOffset = 0.075M * ratio * 1.0m;
+        public decimal limitOffset = 0.018M * 1.0m;
+        public decimal stopOffset = 0.075M * 1.0m;
         public OrderTicket limitOrder;
         public OrderTicket slOrder;
-        //public OrderTicket tpOrder;
+
+        public OrderTicket tpOrder;
         public decimal highestSPYPrice;
         public int bullBear = 1;
         public LimitOrderStopLossTakeProfit algo;
@@ -75,22 +84,36 @@ namespace QuantConnect.Lean.Launcher.CustomCode
 
         public void TPLimitOrder(string _ticker, decimal _quantity, decimal _limitPrice, string _mess)
         {
-            tpquantity = _quantity;
-            tplimitPrice = _limitPrice;
-            tpmess = _mess;
+            if (C.useTPLimit == true)
+            {
+                tpOrder = algo.LimitOrder(_ticker, _quantity, _limitPrice, _mess);
+            }
+            else
+            {
+                tpquantity = _quantity;
+                tplimitPrice = _limitPrice;
+                tpmess = _mess;
+            }
         }
 
         public void TPCancel(string _mess)
         {
-            tpquantity = 0.0m;
-            tplimitPrice = 0.0m;
-            tpmess = _mess;
+            if (C.useTPLimit == true && tpOrder != null)
+            {
+                tpOrder.Cancel(_mess);
+            }
+            else
+            {
+                tpquantity = 0.0m;
+                tplimitPrice = 0.0m;
+                tpmess = _mess;
+            }
         }
 
         public override bool OnOrderEvent(OrderEvent orderEvent)
         {
-            if (orderEvent.Status != OrderStatus.Filled) return false;
-            var close = "close;" + algo.CurrentSlice[ticker].Close.ToString() + ";highestSPYPrice;" + highestSPYPrice.ToString(new CultureInfo("en-us")) + ";";
+            if (orderEvent.Status != OrderStatus.Filled || !algo.CurrentSlice.Where(k => k.Key.Value == ticker).Any()) return false;
+            var close = "close;" + algo.CurrentSlice[ticker].Close.ToString() + ";highestSPYPrice;" + highestSPYPrice.ToString(C.en_us) + ";";
             // Limit Order has been filled, so now create Take Profit and Stop Loss orders
             if (slOrder == null && limitOrder != null && limitOrder.OrderId == orderEvent.OrderId)
             {
@@ -100,7 +123,7 @@ namespace QuantConnect.Lean.Launcher.CustomCode
                 // Create Take Profit Order of opposite amount
                 var quantity = -algo.Securities[ticker].Holdings.Quantity * bullBear;
                 var limitPrice = Math.Round(limitTicket.AverageFillPrice * (1.00m + bullBear * stopOffset * 2.0m), 2);
-                var mess = close + "TP; quantity;" + quantity.ToString(new CultureInfo("en-us")) + " ;limitPrice;" + limitPrice.ToString(new CultureInfo("en-us"));
+                var mess = close + "TP; quantity;" + quantity.ToString(C.en_us) + " ;limitPrice;" + limitPrice.ToString(C.en_us);
                 TPLimitOrder(ticker, quantity, limitPrice, mess);// * 1.5M
                 if (highestSPYPrice == -1)
                 {
@@ -108,10 +131,10 @@ namespace QuantConnect.Lean.Launcher.CustomCode
                 }
                 var quantitySL = -algo.Securities[ticker].Holdings.Quantity * bullBear;
                 var priceSL = Math.Round(highestSPYPrice * (1.00m - stopOffset * bullBear), 2);
-                var mess2 = close + "StopLoss;quantitySL;" + quantitySL.ToString(new CultureInfo("en-us")) + ";priceSL;" + priceSL.ToString(new CultureInfo("en-us"));
+                var mess2 = close + "StopLoss;quantitySL;" + quantitySL.ToString(C.en_us) + ";priceSL;" + priceSL.ToString(C.en_us);
                 slOrder = algo.StopMarketOrder(ticker, quantitySL, priceSL, mess2);
                 limitOrder = null;
-                Report("quantity;" + quantity.ToString(new CultureInfo("en-us")) + " ;limitPrice;" + limitPrice.ToString(new CultureInfo("en-us")));
+                Report("quantity;" + quantity.ToString(C.en_us) + " ;limitPrice;" + limitPrice.ToString(C.en_us));
                 return false;
             }
 
@@ -132,18 +155,27 @@ namespace QuantConnect.Lean.Launcher.CustomCode
             if (slOrder != null && slOrder.OrderId == orderEvent.OrderId)
             {
                 stopMarketOrderFillTime = algo.Time;
-                if (tpquantity != 0.0m)
+                if ((tpquantity != 0.0m && C.useTPLimit == false) ||
+                    (tpOrder != null && C.useTPLimit == true))
                 {
-                    // tpOrder.Cancel(close + "Take profit cancelled due to stop loss id: " + orderEvent.OrderId.ToString(new CultureInfo("en-us")));
+                    // tpOrder.Cancel(close + "Take profit cancelled due to stop loss id: " + orderEvent.OrderId.ToString(C.en_us));
                     // tpOrder = null;
-                    TPCancel(close + "Take profit cancelled due to stop loss id: " + orderEvent.OrderId.ToString(new CultureInfo("en-us")));
+                    TPCancel(close + "Take profit cancelled due to stop loss id: " + orderEvent.OrderId.ToString(C.en_us));
                     //algo.Transactions.CancelOpenOrders(ticker);
                 }
                 return true;
             }
-            Report(";StopLoss");
-            tpquantity = 0.0m;
-            tplimitPrice = 0.0m;
+            //only other possibility is TP order being filled
+            Report(";TakeProfit");
+            if (C.useTPLimit == false)
+            {
+                tpquantity = 0.0m;
+                tplimitPrice = 0.0m;
+            }
+            else
+            {
+                tpOrder = null;
+            }
             tpmess = "";
             if (slOrder != null)
             {
@@ -158,18 +190,22 @@ namespace QuantConnect.Lean.Launcher.CustomCode
 
     public class LimitOrderStopLossTakeProfit : QCAlgorithm
     {
-        public List<string> tickers = new List<string>(new string[] { "DVN" });//"BND", "DVN" positive 2015 - 2020
+        //some choices
+        //"CTSH", 	"AAPL", 	"ANSS", 	"ATVI", 	"ANET", 	"ADBE", 	"CELG", 	"BIIB", 	"GILD", 	"ABMD", 	"UNH", 	"ALXN", 	"BLK", 	"HFC", 	"KSU", 
+        public List<string> tickers = new List<string> { "GILD", "ABMD", "UNH", "ALXN", "BLK", "HFC", "KSU" }; //random stock picks. replace these with your own picks
+        //"ATVI", "ADBE", "CELG", "BIIB", "GILD", "ABMD", "UNH", "ALXN", "BLK", "HFC", "KSU" });//"BND", "DVN" positive 2015 - 2020
+
         public Dictionary<string, OrderState> orders = new Dictionary<string, OrderState>();
         private int noOrderBarCount = 0;
         public override void Initialize()
         {
-            SetStartDate(2006, 1, 1);
-            SetEndDate(2011, 1, 1);
+            SetStartDate(2012, 1, 1);
+            SetEndDate(2020, 10, 1);
             SetCash(100000);
             tickers.ForEach(t =>
             {
                 Equity security = base.AddEquity(t, Resolution.Hour);
-                security.SetDataNormalizationMode(DataNormalizationMode.Raw);
+                // security.SetDataNormalizationMode(DataNormalizationMode.Raw);
                 orders.Add(t, new OrderState(t, security, this));
             });
             SetBenchmark("SPY");
@@ -182,42 +218,46 @@ namespace QuantConnect.Lean.Launcher.CustomCode
         public void OnData(IEnumerable<KeyValuePair<Symbol, BaseData>> slice)
         {
             var self = this;
-            if (!Portfolio.Invested)
+
+            foreach (var ticker in slice.Select(k => k.Key.Value))
             {
-                foreach (var ticker in slice.Select(k => k.Key.Value))
+                var close = "close;" + CurrentSlice[ticker].Close.ToString() + ";highestSPYPrice;" + orders[ticker].highestSPYPrice.ToString(C.en_us) + ";";
+                if (this.Securities[ticker].Holdings.Quantity == 0)
                 {
-                    var close = "close;" + CurrentSlice[ticker].Close.ToString() + ";highestSPYPrice;" + orders[ticker].highestSPYPrice.ToString(new CultureInfo("en-us")) + ";";
-                    if (this.Securities[ticker].Holdings.Quantity == 0)
+                    //if (Portfolio.MarginRemaining > this.Securities[ticker].Close)
+                    //if (this.Securities[ticker].Holdings.Quantity != 0)
+                    if (!Transactions.GetOpenOrders().Where(oo => oo.Symbol.Value.ToUpper(C.en_us) ==
+                        ticker.ToUpper(C.en_us)).Any())
                     {
-                        //if (Portfolio.MarginRemaining > this.Securities[ticker].Close)
-                        //if (this.Securities[ticker].Holdings.Quantity != 0)
-                        if (!Transactions.GetOpenOrders().Any())
+                        var unfilledCount
+                            = orders.Where(o => o.Value.limitOrder != null && o.Value.limitOrder.Status != OrderStatus.Filled).Count();
+                        if (unfilledCount > orders.Keys.Count / 2) unfilledCount = unfilledCount / 3; //make bigger bets until they are filled
+                        if (unfilledCount == 0)
                         {
-                            var quantit = orders[ticker].bullBear * Portfolio.MarginRemaining / (/*this.Securities.Count * */this.Securities[ticker].Close);
-                            var pric = self.Securities[ticker].Price * (1.00M - orders[ticker].stopOffset);
-                            var mess = close + "Initial LimitOrder; quantit;" +
-                                quantit.ToString(new CultureInfo("en-us")) + ";pric;" + pric.ToString(new CultureInfo("en-us"));
-                            //Transactions.CancelOpenOrders(ticker);
-                            orders[ticker].limitOrder = LimitOrder(ticker, quantit, pric, mess);
-                            orders[ticker].TPCancel(close + "Main Limit order submitted, so residual: ");
-                            orders[ticker].highestSPYPrice = -1;
-                            if (orders[ticker].slOrder != null)
-                            {
-                                orders[ticker].slOrder.Cancel(close + "Cancelling stop loss due to new main limit order being placed");
-                                orders[ticker].slOrder = null;
-                            }
-                            orders[ticker].Report(mess);
+                            unfilledCount = 2;
                         }
+                        var quantit = orders[ticker].bullBear * Portfolio.MarginRemaining /
+                            (unfilledCount *
+                            this.Securities[ticker].Close);
+                        var pric = self.Securities[ticker].Price * (1.00M - orders[ticker].stopOffset);
+                        var mess = close + "Initial LimitOrder; quantit;" +
+                            quantit.ToString(C.en_us) + ";pric;" + pric.ToString(C.en_us);
+                        //Transactions.CancelOpenOrders(ticker);
+                        orders[ticker].limitOrder = LimitOrder(ticker, quantit, pric, mess);
+                        orders[ticker].TPCancel(close + "Main Limit order submitted, so residual: ");
+                        orders[ticker].highestSPYPrice = -1;
+                        if (orders[ticker].slOrder != null)
+                        {
+                            orders[ticker].slOrder.Cancel(close + "Cancelling stop loss due to new main limit order being placed");
+                            orders[ticker].slOrder = null;
+                        }
+                        orders[ticker].Report(mess);
                     }
                 }
-            }
-            else
-            {
-                foreach (var ticker in slice.Select(k => k.Key.Value))
+                else
                 {
-                    var close = "close;" + CurrentSlice[ticker].Close.ToString() + ";highestSPYPrice;" + orders[ticker].highestSPYPrice.ToString(new CultureInfo("en-us")) + ";";
                     //Do dynamic TP if price is right.
-                    if (orders[ticker].tpquantity != 0.0m)
+                    if (orders[ticker].tpquantity != 0.0m && C.useTPLimit == false)
                     {
                         if ((orders[ticker].tpquantity < 0.0m && self.Securities[ticker].Price >= orders[ticker].tplimitPrice) ||
                             (orders[ticker].tpquantity > 0.0m && self.Securities[ticker].Price < orders[ticker].tplimitPrice))
@@ -241,7 +281,9 @@ namespace QuantConnect.Lean.Launcher.CustomCode
                         orders[ticker].Report("; updateFields.StopPrice;" + updateFields.StopPrice.ToString());
                     }
                     //test to see if there exists TP quantity and slOrder
-                    if (orders[ticker].tpquantity == 0.0m || orders[ticker].slOrder == null)
+                    if ((C.useTPLimit == false && orders[ticker].tpquantity == 0.0m) ||
+                        (C.useTPLimit == true && orders[ticker].tpOrder == null) ||
+                        orders[ticker].slOrder == null)
                     {
                         if (Portfolio.Invested == true && this.Securities[ticker].Holdings.Quantity != 0)
                         {
@@ -259,10 +301,14 @@ namespace QuantConnect.Lean.Launcher.CustomCode
                     if (noOrderBarCount > 40)
                     {
                         Liquidate(Securities[ticker].Symbol);
-                        Transactions.CancelOpenOrders(ticker);
                         orders[ticker].limitOrder?.Cancel("No main limit order filled in 40 bars");
                         orders[ticker].slOrder?.Cancel("No main limit order filled in 40 bars");
-                        orders[ticker].tpquantity = 0.0m;
+                        Transactions.CancelOpenOrders(ticker);
+                        if (C.useTPLimit == false)
+                        {
+                            orders[ticker].tpquantity = 0.0m;
+                            orders[ticker].tplimitPrice = 0.0m;
+                        }
                         orders[ticker].slOrder = null;
                         orders[ticker].limitOrder = null;
                         noOrderBarCount = 0;
